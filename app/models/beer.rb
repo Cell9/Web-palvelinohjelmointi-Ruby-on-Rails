055750c62ebf4f87f96b3418342bf1ac9989b187
Markus Kononen <43392343+Cell9@users.noreply.github.com>
2022-12-23 05:24:29 +0200

    part 2, ex.4 done

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,13 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        x = 0
+        ratings.each do |rating|
+            x += rating.score
+        end
+    result = x/ratings.count.to_f
+    return result.truncate(2)
+    end
 end
