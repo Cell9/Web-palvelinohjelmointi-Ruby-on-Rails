b95334909e0a03f02319cbe214e53351f91f4fc1
Markus Kononen <43392343+Cell9@users.noreply.github.com>
2022-12-29 01:31:38 +0200

    Part 3, ex 1-2

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,13 +1,16 @@
 class Beer < ApplicationRecord
-    include RatingAverage
+  include RatingAverage
 
-    belongs_to :brewery
-    has_many :ratings, dependent: :destroy
+  belongs_to :brewery
+  has_many :ratings, dependent: :destroy
 
-    def to_s
-        "#{name},  #{brewery.name}"
-     end
+  def to_s
+    "#{name},  #{brewery.name}"
+  end
+
+  def average
+    return 0 if ratings.empty?
+
+    ratings.map(&:score).sum / ratings.count.to_f
+  end
 end
-
-
-#inspiration from https://medium.com/@james.a.hughes/using-the-reduce-method-in-ruby-907f3c18ae1f
--- app/models/concerns/rating_average.rb
+++ app/models/concerns/rating_average.rb
@@ -1,9 +1,8 @@
 module RatingAverage
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-    def average_rating
-      result = ratings.reduce(0) { |sum, rating| sum + rating.score } / ratings.count.to_f
-      return result.truncate(2)
-    end
-
+  def average_rating
+    result = ratings.reduce(0) { |sum, rating| sum + rating.score } / ratings.count.to_f
+    result.truncate(2)
+  end
 end
