ad50043adcb997a751e470784feea5442ea6247c
Markus Kononen <43392343+Cell9@users.noreply.github.com>
2022-12-29 05:27:08 +0200

    part 3, ex. 15

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     include RatingAverage
 
     has_secure_password
-    has_many :ratings
+    has_many :ratings, dependent: :destroy
     has_many :beers, through: :ratings
 
     validates :username, uniqueness: true, 
