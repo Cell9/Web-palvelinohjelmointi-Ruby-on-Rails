6db20e03394826a9add04f78b0e206ce559dc63c
Markus Kononen <43392343+Cell9@users.noreply.github.com>
2022-12-29 23:37:10 +0200

    Part 4, ex.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,19 @@ class User < ApplicationRecord
   validates :username, uniqueness: true,
                        length: { minimum: 3, maximum: 30 }
   validates :password, length: { minimum: 4 }, format: { with: /(?=.*[A-Z])(?=.*[0-9]).*/ }
+
+  def favorite_beer
+    return nil if ratings.empty?   
+
+    #ratings.sort_by{ |r| r.score }.last.beer
+    ratings.order(score: :desc).limit(1).first.beer
+  end
+
+  def favorite_brewery
+    return nil if ratings.empty?   
+
+    favorite = ratings.joins(:beer).group("beers.brewery_id").select("avg(score) as avg, brewery_id").order(avg: :desc).first
+
+    Brewery.find(favorite.brewery_id)    
+  end
 end
